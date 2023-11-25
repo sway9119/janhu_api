a4156901d4590e17ec18f88a6e2a64918c936030
sway <40759948+sway9119@users.noreply.github.com>
2023-11-25 12:20:52 +0900

    Utilsコントローラの追加

---
+++ app/controllers/utils_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# その他の機能
+class UtilsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "home#index"
   root to: "home#index"
 
 end
