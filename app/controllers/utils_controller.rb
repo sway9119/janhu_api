d0713a97a9db138df0b8dfeb5350f047947dacc0
sway <40759948+sway9119@users.noreply.github.com>
2023-11-25 12:22:51 +0900

    agariメソッドの追加

--- app/controllers/utils_controller.rb
+++ app/controllers/utils_controller.rb
@@ -2,4 +2,18 @@
 
 # その他の機能
 class UtilsController < ApplicationController
+  def agari
+    render json:
+    {
+      mentsu: [
+        ['p1', 'p1', 'p1'],
+        ['p1', 'p2', 'p3'],
+        ['s1', 's1', 's1'],
+        ['s1', 's2', 's3'],
+        ['s1', 's2', 's3'],
+      ],
+      janto: ['j1','j1'],
+      agari_type: 'tanki'
+    }
+  end
 end
