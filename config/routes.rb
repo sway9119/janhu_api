9401475bc7a41d6a39a34114624578f49c71278f
sway <40759948+sway9119@users.noreply.github.com>
2023-11-26 12:33:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   get 'agari', to: 'utils#agari'
+
+  tehai = '(/:mentsu)(/:janto)(/:agari_type)(/:menzen)/'
+  get "calc_fu#{tehai}",
+  # constraints: { mentsu: /\d+/, janto: /\d+/, agari_type: /[a-z]+/, menzen /[true|false]/ },
+  to: 'utils#calc_fu',
+  as: 'calc_fu'
 end
