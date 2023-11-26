d5a8ba5ab1d3779ff5e715aa0a797ce03d3e6bd7
sway <40759948+sway9119@users.noreply.github.com>
2023-11-26 12:33:16 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :set_params
+
+  def set_params
+    hash = {}
+    request.query_parameters.each do |key, value|
+     hash[key] = value.to_s
+    end
+    @params = hash
+  end
 end
